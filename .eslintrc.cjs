29201f1c8586870e4700016306d075c7d829db69
CRAFTSW0MAN <105918726+CRAFTSW0MAN@users.noreply.github.com>
2024-05-02 02:52:15 +0600

    feat: add new rules for @typescript-eslint/no-unnecessary-type-assertion

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -24,6 +24,7 @@ module.exports = {
     },
     ecmaVersion: 'latest',
     sourceType: 'module',
+    tsconfigRootDir: __dirname,
   },
   plugins: ['@typescript-eslint', '@stylistic/ts', 'import', 'react', 'react-hooks', 'react-refresh'],
   rules: {
