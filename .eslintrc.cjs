3674809485332afeed1fb74f3ec326204c5770f4
CRAFTSW0MAN <105918726+CRAFTSW0MAN@users.noreply.github.com>
2024-05-02 03:20:33 +0600

    feat: add coverage-v8

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -24,6 +24,7 @@ module.exports = {
     },
     ecmaVersion: 'latest',
     sourceType: 'module',
+    project: "./tsconfig.json",
     tsconfigRootDir: __dirname,
   },
   plugins: ['@typescript-eslint', '@stylistic/ts', 'import', 'react', 'react-hooks', 'react-refresh'],
--- vitest.config.ts
+++ vitest.config.ts
@@ -3,5 +3,15 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     environment: 'jsdom',
+    setupFiles: ['./src/setupTest.ts'],
+    globals: true,
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'json', 'html'],
+      extension: ['ts', 'tsx'],
+      include: ['src'],
+      all: true,
+      exclude: ['**/*.type.ts', '**/*.d.ts', '**/*.interface.ts'],
+    },
   },
 });
