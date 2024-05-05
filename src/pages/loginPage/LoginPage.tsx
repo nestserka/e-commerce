199099f3769363c089d790f00c5785d00809ed75
CRAFTSW0MAN <105918726+CRAFTSW0MAN@users.noreply.github.com>
2024-05-05 15:58:04 +0600

    feat: NASA-02 add folder from pages and add redirects file

---
+++ src/pages/loginPage/LoginPage.tsx
@@ -0,0 +1,9 @@
+import style from './_login.module.scss';
+
+export default function LoginPage(): JSX.Element {
+  return (
+    <section className={style.login} data-testid="login">
+      LoginPage
+    </section>
+  );
+}
---
+++ src/pages/mainPage/MainPage.tsx
@@ -0,0 +1,9 @@
+import style from './_main.module.scss';
+
+export default function MainPage(): JSX.Element {
+  return (
+    <section className={style.main} data-testid="main">
+      MainPage
+    </section>
+  );
+}
