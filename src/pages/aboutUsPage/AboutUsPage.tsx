543421a610935f479fcb05511d833e1c45b78a81
CRAFTSW0MAN <105918726+CRAFTSW0MAN@users.noreply.github.com>
2024-05-05 16:01:17 +0600

    feat: NASA-02 add folder from pages and add redirects file

--- src/pages/aboutUsPage/AboutUsPage.tsx
+++ src/pages/aboutUsPage/AboutUsPage.tsx
@@ -1,7 +1,6 @@
-import style from './_about_us.module.scss'
+import style from './_about_us.module.scss';
 
 export default function AboutUsPage(): JSX.Element {
-
   return (
     <section className={style.about_us} data-testid="about_us">
       AboutUs
