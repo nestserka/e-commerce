6f482af3e6b6cc1443856c5739934ec56590369d
Ania Chebysheva <anna.chebysheva@gmail.com>
2024-05-06 01:04:29 +0200

    feat: NASA-57 add footer component

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,9 +1,20 @@
 import style from './_footer.module.scss';
+import logo from '../../assets/images/rs-logo.svg';
 
-export default function ProfilePage(): JSX.Element {
+export default function Footer(): JSX.Element {
   return (
     <footer className={style.footer} data-testid="footer">
-      Footer
+      <section>
+        <a href="https://rs.school/courses/javascript-mentoring-program" target="_blank">
+          <img src={logo} className="rs-logo" alt="RS School logotype" />
+        </a>
+      </section>
+      <section>
+        <span>Final task</span>
+      </section>
+      <section>
+        <span>2024</span>
+      </section>
     </footer>
   );
 }
