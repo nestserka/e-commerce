dde2b4d2fe2ac5010a8a6e49681a53a400c88d07
Ania Chebysheva <anna.chebysheva@gmail.com>
2024-05-06 01:26:12 +0200

    feat: NASA-57 add header component

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,9 +1,14 @@
-import style from './_header.scss';
+import style from './_header.module.scss';
+import Navigation from '../navigation/Navigation.tsx';
+import logo from '../../assets/images/ns-store-logo.svg';
 
 export default function Header(): JSX.Element {
   return (
     <header className={style.header} data-testid="header">
-      header
+      <section>
+        <img src={logo} className="ns-logo" alt="NASA Store logotype" />
+      </section>
+      <Navigation />
     </header>
   );
 }
