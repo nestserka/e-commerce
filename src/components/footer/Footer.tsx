0c8d5f3b89592be45d07c7029b8e34b3dffdf024
Ania Chebysheva <anna.chebysheva@gmail.com>
2024-05-06 00:50:25 +0200

    feat: NASA-57 add font

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -3,7 +3,7 @@ import style from './_footer.module.scss';
 export default function ProfilePage(): JSX.Element {
   return (
     <footer className={style.footer} data-testid="footer">
-      footer
+      Footer
     </footer>
   );
 }
