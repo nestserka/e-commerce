2e22f5968f2568d06ed2961c10686365a204cc75
Ania Chebysheva <anna.chebysheva@gmail.com>
2024-05-06 01:59:27 +0200

    feat: NASA-57 add navigation markup

--- src/components/navigation/Navigation.tsx
+++ src/components/navigation/Navigation.tsx
@@ -3,7 +3,13 @@ import style from './_navigation.module.scss';
 export default function Navigation(): JSX.Element {
   return (
     <nav className={style.nav} data-testid="navigation">
-      Navigation
+      <ul className={style.navlist}>
+        <li>Home</li>
+        <li>Sign In</li>
+        <li>Sign Up</li>
+        <li>Catalog</li>
+        <li>About</li>
+      </ul>
     </nav>
   );
 }
