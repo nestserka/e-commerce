c04d26eeabb693db822d020dc34e2aedf654277c
CRAFTSW0MAN <105918726+CRAFTSW0MAN@users.noreply.github.com>
2024-05-06 18:49:51 +0600

    Revert "feat: NASA-57 add header component"
    This reverts commit dd65dcc7062775bc496241f21a4ad0f1d7dfff46.

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,14 +1,9 @@
-import style from './_header.module.scss';
-import Navigation from '../navigation/Navigation.tsx';
-import logo from '../../assets/images/ns-store-logo.svg';
+import style from './_header.scss';
 
 export default function Header(): JSX.Element {
   return (
     <header className={style.header} data-testid="header">
-      <section>
-        <img src={logo} className="ns-logo" alt="NASA Store logotype" />
-      </section>
-      <Navigation />
+      header
     </header>
   );
 }
