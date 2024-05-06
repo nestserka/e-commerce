12afb3de83711e80b1d7567980a4f318aa5e3fb9
Ania Chebysheva <anna.chebysheva@gmail.com>
2024-05-06 13:54:53 +0200

    feat: NASA-57 add navigation styles

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -4,15 +4,15 @@ import logo from '../../assets/images/rs-logo.svg';
 export default function Footer(): JSX.Element {
   return (
     <footer className={style.footer} data-testid="footer">
-      <section>
+      <section className={style['footer-section']}>
         <a href="https://rs.school/courses/javascript-mentoring-program" target="_blank">
           <img src={logo} className="rs-logo" alt="RS School logotype" />
         </a>
       </section>
-      <section>
+      <section className={style['footer-section']}>
         <span>Final task</span>
       </section>
-      <section>
+      <section className={style['footer-section']}>
         <span>2024</span>
       </section>
     </footer>
--- src/components/navigation/Navigation.tsx
+++ src/components/navigation/Navigation.tsx
@@ -3,12 +3,12 @@ import style from './_navigation.module.scss';
 export default function Navigation(): JSX.Element {
   return (
     <nav className={style.nav} data-testid="navigation">
-      <ul className={style.navlist}>
-        <li>Home</li>
-        <li>Sign In</li>
-        <li>Sign Up</li>
-        <li>Catalog</li>
-        <li>About</li>
+      <ul className={style['nav-list']}>
+        <li className={style['nav-item']}>Home</li>
+        <li className={style['nav-item']}>Sign In</li>
+        <li className={style['nav-item']}>Sign Up</li>
+        <li className={style['nav-item']}>Catalog</li>
+        <li className={style['nav-item']}>About</li>
       </ul>
     </nav>
   );
