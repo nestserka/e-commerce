1cc4744f1c2ef80a276c42a265678ae2defdfc37
CRAFTSW0MAN <105918726+CRAFTSW0MAN@users.noreply.github.com>
2024-05-06 18:49:51 +0600

    Revert "feat: NASA-02 add folder from pages and add redirects file"
    This reverts commit 199099f3769363c089d790f00c5785d00809ed75.

--- src/components/footer/Footer.tsx
+++
@@ -1,9 +0,0 @@
-import style from './_footer.module.scss';
-
-export default function ProfilePage(): JSX.Element {
-  return (
-    <footer className={style.footer} data-testid="footer">
-      footer
-    </footer>
-  );
-}
--- src/pages/registrationPage.tsx/RegistrationPage.tsx
+++
@@ -1,9 +0,0 @@
-import style from './_registration.module.scss';
-
-export default function RegistrationPage(): JSX.Element {
-  return (
-    <section className={style.registration} data-testid="registration">
-      RegistrationPage
-    </section>
-  );
-}
