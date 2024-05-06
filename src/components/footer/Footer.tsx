50b9b2835e8703472aaf116cc6db8e0eb7cbdfed
CRAFTSW0MAN <105918726+CRAFTSW0MAN@users.noreply.github.com>
2024-05-06 18:49:51 +0600

    Revert "feat: NASA-57 add navigation styles"
    This reverts commit 12afb3de83711e80b1d7567980a4f318aa5e3fb9.

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -4,15 +4,15 @@ import logo from '../../assets/images/rs-logo.svg';
 export default function Footer(): JSX.Element {
   return (
     <footer className={style.footer} data-testid="footer">
-      <section className={style['footer-section']}>
+      <section>
         <a href="https://rs.school/courses/javascript-mentoring-program" target="_blank">
           <img src={logo} className="rs-logo" alt="RS School logotype" />
         </a>
       </section>
-      <section className={style['footer-section']}>
+      <section>
         <span>Final task</span>
       </section>
-      <section className={style['footer-section']}>
+      <section>
         <span>2024</span>
       </section>
     </footer>
