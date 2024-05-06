f752efedcff030e96511915bf089bbb2f7c98e5a
CRAFTSW0MAN <105918726+CRAFTSW0MAN@users.noreply.github.com>
2024-05-06 18:49:51 +0600

    Revert "feat: NASA-57 add navigation markup"
    This reverts commit 2e22f5968f2568d06ed2961c10686365a204cc75.

--- src/components/navigation/Navigation.tsx
+++ src/components/navigation/Navigation.tsx
@@ -3,13 +3,7 @@ import style from './_navigation.module.scss';
 export default function Navigation(): JSX.Element {
   return (
     <nav className={style.nav} data-testid="navigation">
-      <ul className={style.navlist}>
-        <li>Home</li>
-        <li>Sign In</li>
-        <li>Sign Up</li>
-        <li>Catalog</li>
-        <li>About</li>
-      </ul>
+      Navigation
     </nav>
   );
 }
