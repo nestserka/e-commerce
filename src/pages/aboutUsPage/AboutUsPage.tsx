a9fb8259a6d6094b08bdbd2ac322d0c019634613
CRAFTSW0MAN <105918726+CRAFTSW0MAN@users.noreply.github.com>
2024-05-06 18:49:51 +0600

    Revert "feat: NASA-02 add folder from pages and add redirects file"
    This reverts commit e69a00636f7ebb5ead5d6f713b3f65a4d7c978a6.

--- src/pages/aboutUsPage/AboutUsPage.tsx
+++ src/pages/aboutUsPage/AboutUsPage.tsx
@@ -1,6 +1,7 @@
-import style from './_about_us.module.scss';
+import style from './_about_us.module.scss'
 
 export default function AboutUsPage(): JSX.Element {
+
   return (
     <section className={style.about_us} data-testid="about_us">
       AboutUs
