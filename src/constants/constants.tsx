64b01a8246b249080ff584456ff8210ec6d54898
Ania Chebysheva <anna.chebysheva@gmail.com>
2024-05-07 17:00:27 +0200

    refactor: NASA-57 rename a constant

--- src/constants/constants.tsx
+++ src/constants/constants.tsx
@@ -1,6 +1,6 @@
 import type { NavigationLink } from '../components/navigation/types';
 
-export const NAVLINKS: NavigationLink[] = [
+export const NAVIGATION_LINKS: NavigationLink[] = [
   { linkTitle: 'Home', route: '/home' },
   { linkTitle: 'Sign In', route: '/signin' },
   { linkTitle: 'Sign Up', route: '/signup' },
