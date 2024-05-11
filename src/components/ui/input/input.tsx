f4712dc474e49ce43b40678a6e771e402e7f2fb7
Ania Chebysheva <anna.chebysheva@gmail.com>
2024-05-12 01:07:32 +0200

    feat: NASA-39 add static visibility icon to password field

--- src/components/ui/input/input.tsx
+++ src/components/ui/input/input.tsx
@@ -1,6 +1,7 @@
 import { type InputHTMLAttributes, useId } from 'react';
 
 import styles from './_input.module.scss';
+import icon from '../../../assets/images/icons/icon-eye.svg';
 
 interface InputProps {
   inputProps: InputHTMLAttributes<HTMLInputElement>;
@@ -10,13 +11,18 @@ interface InputProps {
 export default function Input({ inputProps, label }: InputProps): JSX.Element {
   const id = useId();
   const inputId = inputProps.id ?? id;
+  console.log(inputProps.type);
 
   return (
     <section className={styles['input-wrapper']}>
       <label htmlFor={inputId} className={styles.label}>
         {label} <span className={styles.required}>*</span>
       </label>
-      <input {...inputProps} className={styles.input} />
+      <section className={styles['input-icon-wrapper']}>
+        <input {...inputProps} className={styles.input} />
+
+        {inputProps.type === 'password' && <img src={icon} className={styles['eye-icon']} alt="" />}
+      </section>
     </section>
   );
 }
