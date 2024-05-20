b46971e74f5b9c53a7124aec2642af57d0e6ca16
nestserka <nechatherina@gmail.com>
2024-05-20 16:43:10 +0900

    feat: NASA-52 add test for error messages

---
+++ src/tests/components/errorMessage/ErrorMessage.test.tsx
@@ -0,0 +1,15 @@
+import { render, screen } from '@testing-library/react';
+import { describe, expect, it } from 'vitest';
+
+import ErrorMessage from '../../../components/errorMessage/ErrorMessage';
+
+describe('ErrorMessage', () => {
+    it('renders error message correctly', () => {
+      const errorMessage = 'This is an error message';
+      render(<ErrorMessage message={errorMessage} />);
+  
+      const errorMessageElement = screen.getByTestId('error-message');
+      expect(errorMessageElement).toBeInTheDocument();
+      expect(errorMessageElement).toHaveTextContent(errorMessage);
+    });
+  });
