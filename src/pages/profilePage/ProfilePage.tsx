8a72b15c4e455ca9b0dabd5083876f8335dc9a58
nestserka <nestserava.k@gmail.com>
2024-05-29 21:22:53 +0900

    feat: NASA-110 add modal window on successful update

--- src/pages/profilePage/ProfilePage.tsx
+++ src/pages/profilePage/ProfilePage.tsx
@@ -6,14 +6,23 @@ import { api } from '../../api/Api';
 import { tokenCache } from '../../api/NasaTokenCache';
 import ProfileAvatar from '../../domain/customer/avatar/profileAvatar';
 import { extractShippingAddresses, formatDateOfBirth } from '../../utils/utils';
-import { useCustomerInfo } from '../../core/state/userState';
+import { showModalMessage, useCustomerInfo } from '../../core/state/userState';
 import ProfileView from '../../domain/customer/profileView/profileView';
 
 import type { Params } from 'react-router-dom';
+import ModalMessage from '../../components/modalMessage/ModalMessage';
+
+const modalMessageSuccessUpdateProps = {
+  type: 'success',
+  title: 'Successful Update',
+  message: 'Your data was updated successfully.',
+};
 
 export default function ProfilePage(): JSX.Element {
   const { customerId }: Readonly<Params<string>> = useParams();
   const { setCustomerInfo, isSet } = useCustomerInfo();
+  const { isShown } = showModalMessage();
+  const { type, title, message } = modalMessageSuccessUpdateProps;
 
   useEffect(() => {
     const fetchCustomer = async (): Promise<void> => {
@@ -55,6 +64,7 @@ export default function ProfilePage(): JSX.Element {
     <section className={style['profile-content']} data-testid="profile">
       <div className={style['profile-content-wrapper']}>
         <ProfileAvatar />
+        {isShown && <ModalMessage type={type} title={title} message={message} />}
         {isSet ? <ProfileView /> : <div className="loading">Loading...</div>}
       </div>
     </section>
