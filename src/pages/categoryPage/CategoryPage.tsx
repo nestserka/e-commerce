e724158461e4dd5cbd92776cb18cb0e97b5b96be
CRAFTSW0MAN <105918726+CRAFTSW0MAN@users.noreply.github.com>
2024-05-31 04:06:57 +0600

    feat: NASA-85-1-3 ineractive products card and changing the product list by category

--- src/pages/categoryPage/CategoryPage.tsx
+++ src/pages/categoryPage/CategoryPage.tsx
@@ -17,7 +17,7 @@ export default function CategoryPage(): JSX.Element {
   const { category }: Readonly<Params<string>> = useParams();
   const [subtree, setSubtree] = useState<Category[]>([]);
   const [productsList, setProductsList] = useState<ClientResponse<ProductProjectionPagedSearchResponse>>();
-  const [subtreeList, setSubtreeList] = useState<string[]>();
+  // const [subtreeList, setSubtreeList] = useState<string[]>();
 
   useEffect(() => {
     if (category) {
@@ -27,31 +27,36 @@ export default function CategoryPage(): JSX.Element {
             setSubtree(response);
           }
 
-          const arrSubtreeNameList: string[] = [];
-          response?.forEach((subtreeData: Category) => {
-            arrSubtreeNameList.push(subtreeData.id);
-          });
-          setSubtreeList(arrSubtreeNameList);
-          const str = arrSubtreeNameList.map((name) => `"${name}"`).join(', ');
-          console.log(str, 'str');
-          ProductList.filterByAttributes(str)
+          // const arrSubtreeNameList: string[] = [];
+          // response?.forEach((subtreeData: Category) => {
+          //   arrSubtreeNameList.push(subtreeData.id);
+          // });
+          // setSubtreeList(arrSubtreeNameList);
+          // const str = arrSubtreeNameList.map((name) => `"${name}"`).join(', ');
+          // console.log(str, 'str');
+          // ProductList.filterByAttributes(str)
+          //   .then((productListData: ClientResponse<ProductProjectionPagedSearchResponse>) => {
+          //     setProductsList(productListData);
+          //   })
+          //   .catch(() => {});
+          // ProductList.filterByAttributes(`subtree("${responseCategory.body.id}")`)
+          //   .then((productListData: ClientResponse<ProductProjectionPagedSearchResponse>) => {
+          //     setProductsList(productListData);
+          //   })
+          //   .catch(() => {});
+        })
+        .catch(() => {});
+
+      ProductList.returnProductsByCategoryKey(category)
+        .then((responseCategory) => {
+          console.log(responseCategory);
+          ProductList.filterByAttributes(`subtree("${responseCategory.body.id}")`)
             .then((productListData: ClientResponse<ProductProjectionPagedSearchResponse>) => {
               setProductsList(productListData);
             })
             .catch(() => {});
         })
         .catch(() => {});
-
-      // ProductList.returnProductsByCategoryKey(category)
-      //   .then((responseCategory) => {
-      //     console.log(responseCategory);
-      //     // ProductList.filterByAttributes(`subtree("${responseCategory.body.id}")`)
-      //     //   .then((productListData: ClientResponse<ProductProjectionPagedSearchResponse>) => {
-      //     //     setProductsList(productListData);
-      //     //   })
-      //     //   .catch(() => {});
-      //   })
-      //   .catch(() => {});
     }
   }, [category]);
 
