c334c33881beae8a6668e242d9a07b9dacea2b9c
Ania Chebysheva <anna.chebysheva@gmail.com>
2024-06-09 15:02:01 +0200

    fix: NASA-131 update login user request to merge carts

--- src/api/me/loginUser.ts
+++ src/api/me/loginUser.ts
@@ -1,11 +1,12 @@
 import { tokenCache } from '../token/MyTokenCache';
-import withPasswordFlow from '../middlewareFlows/withPasswordFlow';
 import { LS_PREFIX } from '../../constants/constants';
+import withRefreshToken from '../middlewareFlows/withRefreshToken';
+import withPasswordFlow from '../middlewareFlows/withPasswordFlow';
 
 import type { ClientResponse, Customer, CustomerSignInResult } from '@commercetools/platform-sdk';
 
 export default async function loginUser(email: string, password: string): Promise<Customer> {
-  tokenCache.clear();
+  // tokenCache.clear();
 
   const anonymousCartId = localStorage.getItem(`anonymousCartId-${LS_PREFIX}`) ?? '';
 
@@ -14,16 +15,33 @@ export default async function loginUser(email: string, password: string): Promis
   const body = {
     email,
     password,
-    anonymousCart: { id: anonymousCartId },
     activeCartSignInMode: 'MergeWithExistingCustomerCart',
     updateProductData: true,
   };
+
+  if (anonymousCartId) {
+    await withRefreshToken(tokenCache.get().refreshToken ?? '')
+      .me()
+      .login()
+      .post({
+        body,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+      .execute();
+  }
+
+  tokenCache.clear();
 
   const response: ClientResponse<CustomerSignInResult> = await withPasswordFlow(email, password, tokenCache)
     .me()
     .login()
     .post({
-      body,
+      body: {
+        email,
+        password,
+      },
       headers: {
         'Content-Type': 'application/json',
       },
