add9ca1aa4b8512faad3f7a940888c2b540edb56
Ania Chebysheva <anna.chebysheva@gmail.com>
2024-06-11 21:32:29 +0200

    feat: NASA-133 add remove from cart api request

---
+++ src/api/me/cart/removeProductFromCart.ts
@@ -0,0 +1,41 @@
+import withRefreshToken from '../../middlewareFlows/withRefreshToken';
+import { tokenCache } from '../../token/MyTokenCache';
+
+import type { Cart, ClientResponse, MyCartUpdateAction } from '@commercetools/platform-sdk';
+
+export default async function removeProductFromCart(
+  cartId: string,
+  productId: string,
+  version: number,
+  quantity?: number | undefined,
+): Promise<Cart> {
+  const token = tokenCache.get().refreshToken;
+
+  const itemData: MyCartUpdateAction = quantity
+    ? {
+        action: 'changeLineItemQuantity',
+        lineItemId: productId,
+        quantity,
+      }
+    : {
+        action: 'removeLineItem',
+        lineItemId: productId,
+      };
+
+  const body = {
+    version,
+    actions: [itemData],
+  };
+
+  const response: ClientResponse<Cart> = await withRefreshToken(token ?? '')
+    .me()
+    .carts()
+    .withId({ ID: cartId })
+    .post({ body })
+    .execute();
+
+  console.log(`Product ${productId} removed from cart with id ${cartId}`);
+  console.log('Updated cart:', response.body);
+
+  return response.body;
+}
