efb1eb556aef00e5e0eaf031a78695758f904a3b
Ania Chebysheva <anna.chebysheva@gmail.com>
2024-06-11 02:24:39 +0200

    feat: NASA-123 replace button with new button component on the product page

--- src/domain/cart/сartToggleButton/cartToggleButton.tsx
+++ src/domain/cart/сartToggleButton/cartToggleButton.tsx
@@ -5,18 +5,38 @@ import { useLoginData } from '../../../core/state/userState';
 import type { PAGES } from '../../../constants/constants';
 
 interface CartToggleButtonProps {
-  productId: string;
+  productId: string | undefined;
   page: keyof typeof PAGES;
 }
 
 export default function CartToggleButton({ productId, page }: CartToggleButtonProps): JSX.Element {
-  const { addProductToCart } = useCartData();
   const { customerId } = useLoginData();
+  const { activeCart, setCart, addProductToCart } = useCartData();
+
+  const handleAddToCart = async (): Promise<void> => {
+    if (!activeCart) {
+      try {
+        await setCart(customerId);
+      } catch (err) {
+        console.log((err as Error).message);
+
+        return;
+      }
+    }
+
+    try {
+      if (productId) {
+        await addProductToCart(productId, customerId);
+      }
+    } catch (err) {
+      console.log((err as Error).message);
+    }
+  };
 
   return (
     <button
       type="button"
-      onClick={() => addProductToCart(productId, customerId)}
+      onClick={handleAddToCart}
       className={page === 'PRODUCT' ? 'button-primary' : style['toggle-cart-button-catalog']}
     >
       Add to Cart
