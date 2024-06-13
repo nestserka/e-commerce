fb039feaa3bce9574edd32dc28d02c11b3251a89
Ania Chebysheva <anna.chebysheva@gmail.com>
2024-06-13 13:21:15 +0200

    feat: NASA-132 add quantity controls

--- src/domain/cart/cartItemLine/cartItemLine.tsx
+++ src/domain/cart/cartItemLine/cartItemLine.tsx
@@ -1,9 +1,12 @@
+import { useState } from 'react';
+
 import style from './_cartItemLine.module.scss';
 import iconDelete from '../../../assets/images/icons/icon-delete.svg';
 import { useCartData } from '../../../core/state/cartState';
 import { useLoginData } from '../../../core/state/userState';
 import { getLineItemsPropsToRemove } from '../../../utils/utils';
 
+import type { ChangeEvent } from 'react';
 import type { CartItemLineProps } from '../../../utils/types';
 
 export default function CartItemLine(props: CartItemLineProps): JSX.Element {
@@ -11,6 +14,17 @@ export default function CartItemLine(props: CartItemLineProps): JSX.Element {
     props;
   const { customerId } = useLoginData();
   const { removeProductFromCart } = useCartData();
+  const [itemQuantity, setItemQuantity] = useState<number | string>(quantity);
+
+  const handleIncrement = (): void => {
+    setItemQuantity(Number(itemQuantity) + 1);
+  };
+
+  const handleDecrement = (): void => {
+    if (Number(itemQuantity) > 1) {
+      setItemQuantity(Number(itemQuantity) - 1);
+    }
+  };
 
   const handleRemoveClick = async (): Promise<void> => {
     try {
@@ -18,6 +32,28 @@ export default function CartItemLine(props: CartItemLineProps): JSX.Element {
       await removeProductFromCart(action, customerId);
     } catch (err) {
       console.log('Failed to remove product from the cart', err);
+    }
+  };
+
+  const handleInputChange = (event: ChangeEvent<HTMLInputElement>): void => {
+    const { value } = event.target;
+
+    if (value === '') {
+      setItemQuantity('');
+    } else if (value === '0') {
+      setItemQuantity(0);
+    } else {
+      const parsedValue = parseInt(value, 10);
+
+      if (!Number.isNaN(parsedValue) && parsedValue > 0) {
+        setItemQuantity(parsedValue);
+      }
+    }
+  };
+
+  const handleInputBlur = (event: React.FocusEvent<HTMLInputElement>): void => {
+    if (event.target instanceof HTMLInputElement && (event.target.value === '' || event.target.value === '0')) {
+      setItemQuantity(1);
     }
   };
 
@@ -35,11 +71,17 @@ export default function CartItemLine(props: CartItemLineProps): JSX.Element {
         {discountedPricePerItem && <p className={style['old-price-value']}>{pricePerItem}</p>}
       </section>
       <section className={style['quantity-input-wrapper']}>
-        <button type="button" className={style['quantity-input-control']}>
+        <button type="button" className={style['quantity-input-control']} onClick={handleDecrement}>
           –
         </button>
-        <p className={style['quantity-number']}>{quantity}</p>
-        <button type="button" className={style['quantity-input-control']}>
+        <input
+          type="text"
+          value={itemQuantity}
+          onChange={handleInputChange}
+          onBlur={handleInputBlur}
+          className={style['quantity-input']}
+        />
+        <button type="button" className={style['quantity-input-control']} onClick={handleIncrement}>
           +
         </button>
       </section>
@@ -49,7 +91,7 @@ export default function CartItemLine(props: CartItemLineProps): JSX.Element {
       <button
         type="button"
         className={style['remove-item-btn']}
-        aria-label="Rremove product from cart"
+        aria-label="Remove product from cart"
         onClick={handleRemoveClick}
       >
         <img src={iconDelete} alt="Edit" className={style['icon-delete']} />
