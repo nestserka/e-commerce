90cd96180f0c77d55feddb6893d1846e3dc7b44a
CRAFTSW0MAN <105918726+CRAFTSW0MAN@users.noreply.github.com>
2024-06-20 22:00:06 +0600

    fix: NASA-04-04  fix style

--- src/domain/about/modalAbout/ModalAbout.tsx
+++ src/domain/about/modalAbout/ModalAbout.tsx
@@ -9,7 +9,7 @@ export interface ModalFromAboutPageProps {
 export default function ModalFromAboutPage({ isOpen, children }: ModalFromAboutPageProps): JSX.Element {
   return (
     <>
-      <button type="button" className={styles.overlay} onClick={isOpen} aria-label="Close Modal" />
+      <div role="presentation" className={styles.overlay} onClick={isOpen} aria-label="Close Modal" />
       <section className={styles.modal}>
         <button type="button" className={styles['modal-close-btn']} onClick={isOpen}>
           <img src={closeIcon} alt="Edit" />
